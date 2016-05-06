b83952bc53e40df6395dd720d4241b7720465ebd
Torstein Bjørnstad <torstein.bjornstad@nrk.no>
2016-05-06 13:39:45 +0200

    update versions

--- build-docker.sh
+++ build-docker.sh
@@ -15,12 +15,12 @@ RUNNING_DIR=$(dirname $0)
 WORKSPACE="$BASEDIR"
 DOCKER_ROOT="$WORKSPACE/$RUNNING_DIR"
 
-IMAGE_NAME=docker.nrk.no:5000/origo/node4_npm2
+IMAGE_NAME=docker.nrk.no/origo/node4_npm2
 
 IMAGE_TAG=$(image_tag)
 IMAGE_ID="$IMAGE_NAME:$IMAGE_TAG"
 
-echo "Building Docker image $IMAGE_NAME from $DOCKER_ROOT."
+echo "Building Docker image $IMAGE_NAME with tag $IMAGE_TAG from $DOCKER_ROOT."
 
 docker build -t "$IMAGE_ID" "$DOCKER_ROOT" || exit 1
 docker push "$IMAGE_ID" || exit 1
